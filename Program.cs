5387972a3b950695a9bb01e1c0dab126000b834f
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-19 02:24:19 -0300

    Este programa verifica se a senha é igual ao nome cadastrado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome?");
+            string resposta = Console.ReadLine();
+            Console.WriteLine("Digite uma nova senha");
+            string resposta1 = Console.ReadLine();
+            while(resposta==resposta1){
+                Console.WriteLine("Tente novamente!A senha deve ser diferente do nome");
+                Console.WriteLine("Digite uma nova senha");
+                resposta = Console.ReadLine();
+            }
+
+            Console.WriteLine("Sua senha foi cadastrada com sucesso!");
+        }
+    }
+}
